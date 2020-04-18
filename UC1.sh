3ecb0db475013124b1337a89f4f363710006b718
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-18 13:40:09 +0530

    Refactor. Adding [UseCase7] store the dictionary values into array

--- UC1.sh
+++ UC1.sh
@@ -3,9 +3,25 @@
 read -p "Enter the a value:" a
 read -p "Enter the b value:" b
 read -p "Enter the c value:" c
-declare -A UC
-UC[2]=$(( $a + $b * $c ))
-UC[3]=$(( $a * $b + $c ))
-UC[4]=$(( $c + $a / $b ))
-UC[5]=$(( $a % $b + $c ))
-echo "UseCases" ${UC[@]}
+declare -A useCase
+compute1=$(( $a + $b * $c ))
+compute2=$(( $a * $b + $c ))
+compute3=$(( $c + $a / $b ))
+compute4=$(( $a % $b + $c ))
+
+useCase[2]=$compute1;
+useCase[3]=$compute2;
+useCase[4]=$compute3;
+useCase[5]=$compute4;
+
+echo arrayoutput
+counter=0;
+UseCase[((counter++))]=$compute1;
+UseCase[((counter++))]=$compute2;
+UseCase[((counter++))]=$compute3;
+UseCase[((counter++))]=$compute4;
+echo ${UseCase[@]}
+
+echo dictionaryoutput
+echo ${useCase[@]}
+echo ${!useCase[@]}
