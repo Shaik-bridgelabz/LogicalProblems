94d77eb404c1a7e07d64f99277e5a49247931b66
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-18 13:01:03 +0530

    Refactor. Adding [UseCase6] to store results in a Dictionary

--- UC1.sh
+++ UC1.sh
@@ -3,12 +3,9 @@
 read -p "Enter the a value:" a
 read -p "Enter the b value:" b
 read -p "Enter the c value:" c
-
-UC2=$(( $a + $b * $c ))
-UC3=$(( $a * $b + $c ))
-UC4=$(( $c + $a / $b ))
-UC5=$(( $a % $b + $c ))
-echo $UC2
-echo $UC3
-echo $UC4
-echo $UC5
+declare -A UC
+UC[2]=$(( $a + $b * $c ))
+UC[3]=$(( $a * $b + $c ))
+UC[4]=$(( $c + $a / $b ))
+UC[5]=$(( $a % $b + $c ))
+echo "UseCases" ${UC[@]}
