218c149243bd52a5ef67688ed08f11a2b20a036d
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-19 14:10:22 +0530

    Refactor. Adding [UseCase2] to store Singlet Combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,39 @@
 #!/bin/bash -x
-read -p "Press 1 to flip a coin: " flip
-if [ $flip -eq 1 ]
-then
-rand=$(( RANDOM % 2 ));
-if [ $rand -eq 1 ]
-then
-        echo heads;
-else
-        echo tails;
-fi
-fi
+read -p "Enter how many times to flip " flips
+coins=1;
+
+declare -A flipstore
+heads=0;
+tails=0;
+
+function flip() {
+for (( i=1;i<=$flips;i++ ))
+do
+	s=""
+	for(( j=0;j<$coins;j++ ))
+	do
+		value=$(( RANDOM % 2 ))
+		if [ $value -eq 1 ]
+			then
+				s=$s"H"
+				heads=$(( $heads+1 ))
+		else
+			s=$s"T"
+			tails=$(( $tails+1 ))
+		fi
+	done
+echo final key : $s
+
+flipstore["$s"]=$(( ${flipstore["$s"]} + 1 ))
+done
+}
+
+flip
+
+echo ${!flipstore[@]}
+echo ${flipstore[@]}
+
+MaxValueCoin=$( printf "%s\n" ${!flipstore[@]} ${flipstore[@]} | sort -nr | head -1 )
+percOfMaxCoin=$(( $MaxValueCoin * 100 / $flips ))
+echo Percentage is
+echo $percOfMaxCoin
