51cc2c2ee308e49b4f049ca5ea8566315c70a455
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-20 16:53:53 +0530

    Refactor. Adding [UseCase5] to know the days won and lost

--- Gambler.sh
+++ Gambler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 declare -A totalAmount
-for (( days=1;days<=20;days++ ))
+for (( days=1;days<=5;days++ ))
 do
 	stake=100;
 	bet=1;
@@ -9,29 +9,31 @@ do
 	noOfWins=0;
 	noOfLoss=0;
 	total=0;
+	dayWin=0;
+	dayLoss=0;
 	((stakes++))
 	while (( $stake<$goalPerDay )) && (( $stake>50 ))
 	do
 		value=$(( RANDOM % 2 ))
 		if [ $value -eq 1 ]
 			then
-				noOfWins=$(( $noOfWins + 1 ))
 				stake=$(( $stake + 1 )); #won
 			else
-				noOfLoss=$(( $noOfLoss + 1 ))
 				stake=$(( $stake - 1 )); #lost
 		fi
 	done
 	if [ $stake -ge 150 ]
 		then
+			dayWin=$(($dayWin+1));
 			echo "U have won 50% of stake, u should resign for the day "
 			echo "Stake after winning is "$stake
 		else
+			dayLoss=$(($dayLoss+1));
 			echo "U have lost 50% of stake, u should resign for the day"
 			echo "Stake after Loosing is "$stake
 	fi
-	wins[$days]=$noOfWins
-	loss[$days]=$noOfLoss
+	echo "No of Days won is $dayWin"
+	echo "No of days loss is $dayLoss"
 	total=$(( $total + $stake ))
 	totalAmount["$stake"]=$total
 done
