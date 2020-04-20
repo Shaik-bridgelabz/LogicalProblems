f875bb07357a61c92df6204d02c92776844034bf
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-20 14:54:26 +0530

    Refactor. modifying [Usecase3] to calculate won or lost 50%

--- Gambler.sh
+++ Gambler.sh
@@ -2,23 +2,23 @@
 
 stake=100;
 bet=1;
-wins=0;
-loss=0;
-while (( $wins<=50 )) && (( $loss<=50 ))
+noOfDays=20;
+goalPerDay=150;
+while (( $stake<$goalPerDay )) && (( $stake>50 ))
 do
 	value=$(( RANDOM % 2 ))
 	if [ $value -eq 1 ]
 	then
-		wins=$(( $wins + 1 ));
+		stake=$(( $stake + 1 )); #won
 	else
-		loss=$(( $loss + 1 ));
+		stake=$(( $stake - 1 )); #lost
 	fi
 done
-if [ $wins -gt 50 ]
+if [ $stake -ge 150 ]
 then
 	echo "U have won 50% of stake, u should resign for the day "
+	echo "Stake after winning is "$stake
 else
 	echo "U have lost 50% of stake, u should resign for the day"
+	echo "Stake after Loosing is "$stake
 fi
-echo "Wins= " $wins
-echo "Loss= " $loss
