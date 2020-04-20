ed570077fb10959dff7a9fb692d19c04e5323b4d
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-20 11:16:55 +0530

    Adding [UseCase2] to check win or loose

--- Gambler.sh
+++ Gambler.sh
@@ -2,3 +2,17 @@
 
 stake=100;
 bet=1;
+wins=0;
+loss=0;
+for(( i=1;i<=$stake;i++ ))
+do
+	value=$(( RANDOM % 2 ))
+	if [ $value -eq 1 ]
+	then
+		wins=$(( $wins + 1 ));
+	else
+		loss=$(( $loss + 1 ));
+	fi
+done
+echo "Wins= " $wins
+echo "Loss= " $loss
