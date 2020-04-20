92d9b4b2bf4e604635840e25725efb35475fec73
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-20 11:26:47 +0530

    Adding [UseCase3] to check win or loose 50% of stake

--- Gambler.sh
+++ Gambler.sh
@@ -4,7 +4,7 @@ stake=100;
 bet=1;
 wins=0;
 loss=0;
-for(( i=1;i<=$stake;i++ ))
+while (( $wins<=50 )) && (( $loss<=50 ))
 do
 	value=$(( RANDOM % 2 ))
 	if [ $value -eq 1 ]
@@ -14,5 +14,11 @@ do
 		loss=$(( $loss + 1 ));
 	fi
 done
+if [ $wins -gt 50 ]
+then
+	echo "U have won 50% of stake, u should resign for the day "
+else
+	echo "U have lost 50% of stake, u should resign for the day"
+fi
 echo "Wins= " $wins
 echo "Loss= " $loss
