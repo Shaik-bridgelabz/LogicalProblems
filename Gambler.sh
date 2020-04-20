dec9b45cbc759fdce022fc90715992052aee0dd6
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-20 16:41:17 +0530

    Refactor. Adding [UseCase4] to play for 20 days

--- Gambler.sh
+++ Gambler.sh
@@ -1,24 +1,38 @@
 #!/bin/bash -x
-
-stake=100;
-bet=1;
-noOfDays=20;
-goalPerDay=150;
-while (( $stake<$goalPerDay )) && (( $stake>50 ))
+declare -A totalAmount
+for (( days=1;days<=20;days++ ))
 do
-	value=$(( RANDOM % 2 ))
-	if [ $value -eq 1 ]
-	then
-		stake=$(( $stake + 1 )); #won
-	else
-		stake=$(( $stake - 1 )); #lost
+	stake=100;
+	bet=1;
+	noOfDays=20;
+	goalPerDay=150;
+	noOfWins=0;
+	noOfLoss=0;
+	total=0;
+	((stakes++))
+	while (( $stake<$goalPerDay )) && (( $stake>50 ))
+	do
+		value=$(( RANDOM % 2 ))
+		if [ $value -eq 1 ]
+			then
+				noOfWins=$(( $noOfWins + 1 ))
+				stake=$(( $stake + 1 )); #won
+			else
+				noOfLoss=$(( $noOfLoss + 1 ))
+				stake=$(( $stake - 1 )); #lost
+		fi
+	done
+	if [ $stake -ge 150 ]
+		then
+			echo "U have won 50% of stake, u should resign for the day "
+			echo "Stake after winning is "$stake
+		else
+			echo "U have lost 50% of stake, u should resign for the day"
+			echo "Stake after Loosing is "$stake
 	fi
+	wins[$days]=$noOfWins
+	loss[$days]=$noOfLoss
+	total=$(( $total + $stake ))
+	totalAmount["$stake"]=$total
 done
-if [ $stake -ge 150 ]
-then
-	echo "U have won 50% of stake, u should resign for the day "
-	echo "Stake after winning is "$stake
-else
-	echo "U have lost 50% of stake, u should resign for the day"
-	echo "Stake after Loosing is "$stake
-fi
+	echo ${totalAmount[@]}
